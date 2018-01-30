76b0b386bccf9d028f48236e61f1d5d9eef5ec5d
Rachel <32782380+elra2@users.noreply.github.com>
2018-01-30 16:02:37 +0000

    Update WM.cpp

--- WM.cpp
+++ WM.cpp
@@ -5,6 +5,11 @@ door::door(unsigned char map){
 	port_map = map;
 }
 
+bool door::dooropen(){
+	//open door command
+	doorstatus = 1;
+	return doorstatus;
+}
 
 //BUZZER FUNCTIONS
 buzzer::buzzer(unsigned char map){
