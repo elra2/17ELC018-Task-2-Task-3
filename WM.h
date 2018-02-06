7ff41dd24fda1b5b7bffc18400b15c6948eac074
Rachel <32782380+elra2@users.noreply.github.com>
2018-02-06 15:32:28 +0000

    Add files via upload

--- WM.h
+++ WM.h
@@ -1,4 +1,3 @@
-
 // Definitions for the Device Drivers on STM32F3 Discovery Board
 // DJM 23/01/2018
 
@@ -6,11 +5,54 @@
 #ifndef __WM_H
 #define __WM_H
 
-#include "stdint.h" 
+#include "stdint.h"
+#include "STM.h"
 
+/* 
+  washing machine outputs
+  -----------------------
+  PC6  Buzzer		
+  PD8  7 segment display bit A
+  PD10 7 segment display bit C
+  PD11 7 segment display bit B
+  PD12 motor control	
+  PD13 7 segment display bit D
+  PD14 reset switches
+  PD15 motor direction
+
+  washing machine inputs
+  ----------------------
+  PE8  programme select 1
+  PE9  programme select 2
+  PE10 programme select 3
+  PE11 door open/close
+  PE12 accept switch
+  PE15 motor speed feedback
+*/
+
+// port base addresses
 #define GPIO_C_BASE_ADDR  0x40000000 + 0x08000000 + 0x00000800
 #define GPIO_D_BASE_ADDR  0x40000000 + 0x08000000 + 0x00000C00
 #define GPIO_E_BASE_ADDR  0x40000000 + 0x08000000 + 0x00001000
+
+// port register addresses
+#define GPIO_C_MODE  (GPIO_C_BASE_ADDR)
+#define GPIO_C_SPEED (GPIO_C_BASE_ADDR + 0x08)
+#define GPIO_C_PULL  (GPIO_C_BASE_ADDR + 0x0C)
+#define GPIO_C_IDR   (GPIO_C_BASE_ADDR + 0x10)
+#define GPIO_C_ODR   (GPIO_C_BASE_ADDR + 0x14)
+
+#define GPIO_D_MODE  (GPIO_D_BASE_ADDR)
+#define GPIO_D_SPEED (GPIO_D_BASE_ADDR + 0x08)
+#define GPIO_D_PULL  (GPIO_D_BASE_ADDR + 0x0C)
+#define GPIO_D_IDR   (GPIO_D_BASE_ADDR + 0x10)
+#define GPIO_D_ODR   (GPIO_D_BASE_ADDR + 0x14)
+
+#define GPIO_E_MODE  (GPIO_E_BASE_ADDR)
+#define GPIO_E_SPEED (GPIO_E_BASE_ADDR + 0x08)
+#define GPIO_E_PULL  (GPIO_E_BASE_ADDR + 0x0C)
+#define GPIO_E_IDR   (GPIO_E_BASE_ADDR + 0x10)
+#define GPIO_E_ODR   (GPIO_E_BASE_ADDR + 0x14)
 
 // define the structure of the ports and port access operations
 class GPIOs
@@ -33,56 +75,50 @@ class GPIOs
 class door{
 	public:
 	door(unsigned char);
-	void opendoor();
-	void closedoor();
+	bool GetDoorStatus();
 	private:
-        unsigned char port_map;
+  unsigned char port_map;
 	bool doorstatus;
 };
 
 class buzzer {
 	public:
 	buzzer(unsigned char);
-	bool soundbuzzer();
+	bool GetBuzzerStatus();
+	bool SetBuzzer();
 	private:
-	unsigned char port_map
+	unsigned char port_map;
 	bool buzzerstatus;
 };
 
-//class motor{
-//	public:
-//};
+class programswitches
+{
+	public:
+		programswitches(unsigned char);
+		bool GetSwitches();
+	private:
+		unsigned char port_map;
+		bool switchvalue;
+};
 
+class acceptcancelswitches
+{
+	public:
+		acceptcancelswitches(unsigned char);
+	bool GetSwitches();
+	private:
+		unsigned char port_map;
+	bool switchvalue;
+};
 
-
-// pointer to port E
+// pointer to port E // (0x48001000)
 extern GPIOs *GPIO_E;
+//pointer to port C // (0x48000800)
+extern GPIOs *GPIO_C; 
+//pointer to port D // (0x48000C00)
+extern GPIOs *GPIO_D;
 
-// define a pointer to the parallel port -- from lecure notes
-struct ParPort *PPort= (struct ParPort*) 0x08001000;
 
-
-
-//mapping of buzzer to port C
-#define BUZZER 0x0040 //BIT 6
-
-//mapping of outputs to port D
-#define MOTOR_DIRECTION 0x8000
-#define SWITCH_RESET 0x4000
-#define SEVENSEG_A 0x0100
-#define SEVENSEG_B 0x0800
-#define SEVENSEG_C 0x0400
-#define SEVENSEG_D  0x2000
-#define MOTOR_CONTROL 0x1000
-
-// mapping of Inputs to port E
-#define DOOR 0x0800
-#define PROGRAM_1 0x0100
-#define PROGRAM_2 0x0200
-#define PROGRAM_3 0x0400
-#define ACCEPT 0x1000
-#define CANCEL 0x2000
-#define MOTOR_SPEED 0x8000
 
 
 #endif /* __WM_H */
